55102c6a42801638e73c125ede5da3a7eb1e7af7
Cesar Mayta <cesarmayta@gmail.com>
2021-09-29 10:18:43 -0500

    contenido de lab05

--- lab05/lab05/settings.py
+++ lab05/lab05/settings.py
@@ -74,12 +74,18 @@ WSGI_APPLICATION = 'lab05.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+#DATABASES = {
+ #   'default': {
+  #      'ENGINE': 'django.db.backends.sqlite3',
+   #     'NAME': BASE_DIR / 'db.sqlite3',
+   # }
+#}
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+      'default': {
+          'ENGINE': 'djongo',
+          'NAME': 'tiendatecsup',
+      }
+  }
 
 
 # Password validation
